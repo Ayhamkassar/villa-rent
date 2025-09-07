d5f4c65d5c57e817a515f0905bfee7c598432f4f
Ayhamkassar <kassaraeham067@gmail.com>
2025-09-07 23:47:02 +0300

    fixed

--- server/Models/bookings.js
+++ server/Models/bookings.js
@@ -14,4 +14,4 @@ const bookingSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Booking', bookingSchema);
+module.exports = mongoose.model('bookings', bookingSchema);
